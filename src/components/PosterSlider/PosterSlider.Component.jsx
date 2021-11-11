6d98a8b566f0c315fe9b62515aa72d525455b162
Selvamathi T <65343953+selvamathi@users.noreply.github.com>
2021-11-11 21:34:14 +0530

    made a few changes

--- src/components/PosterSlider/PosterSlider.Component.jsx
+++ src/components/PosterSlider/PosterSlider.Component.jsx
@@ -1,59 +1,71 @@
-import React from 'react';
-import Slider from 'react-slick';
-import Poster from '../Poster/Poster.Component';
+import React from "react";
+import Slider from "react-slick";
+import Poster from "../Poster/Poster.Component";
 
 const PosterSlider = (props) => {
-   
-    const settings= {
-        infinity:false,
-        autoplay: false,
+    const settings = {
+        infinite: false,
+        speed: 500,
         slidesToShow: 5,
         slidesToScroll: 4,
-        InitialSlide: 0,
+        initialSlide: 0,
         responsive: [
             {
-                breakpoints: 1024,
+                breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 2,
-                    infinite:true
+                    slidesToScroll: 3,
+                    infinite: true,
                 },
             },
             {
-                breakpoints: 600,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    InitialSlide:true,
-                },
-
-                breakpoints: 480,
+                breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
-                    
+                    initialSlide: 2,
                 },
             },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
 
-        ]
-    }
+    const { posters, title, subtitle, isDark, config } = props;
 
-    const {posters,title,subtitle,isDark, config}= props;
-    const currentSettings = config ? config :settings;
-    return(
+    const currentSettings = config ? config : settings;
+
+    return (
         <>
-            <div className="flex flex-col items-start my-2">
-                <h3 className={`text-2xl font-bold ${isDark ? "text-white" : "text-black"}`}>{title}</h3>
-                <p className= {`text-sm ${isDark? "text-white": "text-gray-800"}`}>{subtitle}</p>
+            <div className="flex flex-col items-start sm:ml-3 ml-0 my-2">
+                <h3
+                    className={`text-2xl font-bold ${
+                        isDark ? "text-white" : "text-black"
+                    }`}
+                >
+                    {title}
+                </h3>
+                <p
+                    className={`text-sm  ${
+                        isDark ? "text-white" : "text-gray-800"
+                    }`}
+                >
+                    {subtitle}
+                </p>
             </div>
-            <Slider {...currentSettings} >
-                {posters.map((each)=>(
-                    <Poster {...each} isDark={isDark}/>
+            <Slider {...currentSettings}>
+                {posters.map((each) => (
+                    <Poster {...each} isDark={isDark} />
                 ))}
+                {console.log(posters)}
             </Slider>
-        </> 
-    )
-
-}
+        </>
+    );
+};
 
 export default PosterSlider;
